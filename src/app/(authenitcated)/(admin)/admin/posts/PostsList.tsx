943a8ab295f32e88751c2726aac8d626d5d5ce0f
Suryansh Anand <spirit@anandsur.com>
2024-07-28 17:39:53 +0530

    feat: user view of posts

--- src/app/(authenitcated)/(admin)/admin/posts/PostsList.tsx
+++ src/app/(authenitcated)/(admin)/admin/posts/PostsList.tsx
@@ -7,7 +7,14 @@ import {
   CardHeader,
 } from "~/components/ui/card";
 import { api } from "~/trpc/react";
-import { postTypeLabels } from "~/lib/constants";
+import {
+  PostTypeBgColors,
+  PostTypeColors,
+  postTypeLabels,
+} from "~/lib/constants";
+import { Badge } from "~/components/ui/badge";
+import { cn } from "~/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 function PostsList({ companyId }: { companyId: string }) {
   const [postsList] = api.post.getAllByCompanyId.useSuspenseQuery({
@@ -21,11 +28,31 @@ function PostsList({ companyId }: { companyId: string }) {
           {postsList.map((p) => (
             <li key={p.id}>
               <Card className="mx-auto w-full max-w-2xl md:w-3/4">
-                <CardHeader className="flex flex-row items-center justify-between">
+                <CardHeader className="flex flex-row items-center gap-8">
+                  <span className="flex flex-col items-center gap-1">
+                    <Avatar>
+                      <AvatarImage
+                        src={p.user.image ?? "/fallbackAvatar.avif"}
+                      />
+                      <AvatarFallback>U</AvatarFallback>
+                    </Avatar>
+                    <span className="text-xs font-medium">{p.user.name}</span>
+                  </span>
+
                   <p className="text-xl font-semibold">{p.title}</p>
                 </CardHeader>
+
                 <CardContent>{p.content}</CardContent>
-                <CardFooter>{postTypeLabels[p.tags]}</CardFooter>
+                <CardFooter>
+                  <Badge
+                    className={cn(
+                      PostTypeBgColors[p.tags],
+                      PostTypeColors[p.tags],
+                    )}
+                  >
+                    {postTypeLabels[p.tags]}
+                  </Badge>
+                </CardFooter>
               </Card>
             </li>
           ))}
--- src/app/(authenitcated)/(user)/[companyName]/posts/page.tsx
+++ src/app/(authenitcated)/(user)/[companyName]/posts/page.tsx
@@ -1,5 +1,32 @@
-function Page() {
-  return <div>POSTS</div>;
-}
+import { api, HydrateClient } from "~/trpc/server";
+import PostsList from "~/app/(authenitcated)/(admin)/admin/posts/PostsList";
 
-export default Page;
+export default async function Page({
+  params,
+}: {
+  params: { companyName: string };
+}) {
+  const company = await api.company.getCompanyByName({
+    companyName: params.companyName,
+  });
+  void api.post.getAllByCompanyId.prefetch({
+    companyId: company?.id ?? "",
+  });
+
+  return (
+    <>
+      <h2 className="mt-4 scroll-m-20 text-center text-3xl font-semibold tracking-tight">
+        Our Posts
+      </h2>
+      <p className="mb-10 text-center text-gray-400">
+        Know what we have been shipping lately
+      </p>
+
+      <div className="flex flex-col gap-10 md:flex-row">
+        <HydrateClient>
+          <PostsList companyId={company?.id ?? ""} />
+        </HydrateClient>
+      </div>
+    </>
+  );
+}
